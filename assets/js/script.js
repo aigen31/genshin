85f6b39826b900e4f62ebfd1484b1cafcd318c09
aigen31 <aigen.chernogorsk@yandex.ru>
2022-04-27 18:29:06 +0700

    release

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,291 +1,366 @@
 let btn = document.querySelector('#btn'),
-    wrapper = document.querySelector('.character');
+    wrapper = document.querySelector('.character'),
+    imgHead = document.querySelector('.section-main__img')
+    prevShow = document.querySelector('#prev-show'),
+    prevWrapper = document.querySelector('#prev-show-wrapper');
 
 function random(max) {
   let num = Math.floor(Math.random() * max)
   return num;
 }
 
-// let character = {
-//   "members": [
-//     {
-//       "name": "Е Лань",
-//       "url": "https://genshin-info.ru/upload/resize_cache/iblock/c98/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/E-lan.png"
-//     },
-//     {
-//       "name": "Куки",
-//       "url": "https://genshin-info.ru/upload/resize_cache/iblock/ea7/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/kuki.png"
-//     },
-//     {
-//       "name": "Аято",
-//       "url": "https://genshin-info.ru/upload/resize_cache/iblock/e95/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/ayato.png"
-//     },
-//     {
-//       "name": "Яэ Мико",
-//       "url": "https://genshin-info.ru/upload/resize_cache/iblock/a76/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/YAe-Miko.png"
-//     },
-//   ]
-// }
-
-/**
-* Спарсил имена, пока костыль
-**/
-
-let characterName = [
-    'Е Лань',
-    'Куки',
-    'Аято',
-    'Яэ Мико',
-    'Шэнь Хэ',
-    'Юнь Цзинь',
-    'Итто',
-    'Горо',
-    'Тома',
-    'Кокоми',
-    'Райдэн',
-    'Элой',
-    'Сара',
-    'Ёимия',
-    'Саю',
-    'Путешественник Электро',
-    'Аяка',
-    'Кадзуха',
-    'Эола',
-    'Янь Фэй',
-    'Розария',
-    'Ху Тао',
-    'Сяо',
-    'Гань Юй',
-    'Альбедо',
-    'Чжун Ли',
-    'Синь Янь',
-    'Тарталья',
-    'Диона',
-    'Кли',
-    'Венти',
-    'Ци Ци',
-    'Мона',
-    'Кэ Цин',
-    'Дилюк',
-    'Джинн',
-    'Эмбер',
-    'Чун Юнь',
-    'Фишль',
-    'Сян Лин',
-    'Син Цю',
-    'Сахароза',
-    'Рэйзор',
-    'Ноэлль',
-    'Нин Гуан',
-    'Лиза',
-    'Кэйа',
-    'Бэй Доу',
-    'Беннет',
-    'Барбара',
-    'Путешественник Гео',
-    'Путешественник Анемо'
-]
-
-let characterUrl = [
-    [
-        '/upload/resize_cache/iblock/c98/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/E-lan.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/ea7/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/kuki.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/e95/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/ayato.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/a76/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/YAe-Miko.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/148/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/SHen-KHe.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/616/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/YUn-TSzin.png'
-    ],
-    [
-        '/upload/resize_cache/iblock/050/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/itto.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/8db/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/gorou.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/b38/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/thoma.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/bb4/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/kokomi.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/b57/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/shougun.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/013/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/aloy.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/2df/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/sara.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/dad/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/yoimiya.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/402/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/sayu.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/1b2/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Traveler.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/45f/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/ayaka.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/5a8/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/kazuha.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/8c1/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/eula.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/bae/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/feiyan.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/edf/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/rosaria.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/3ac/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/hutao.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/ec1/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/xiao.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/49b/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/ganyu.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/bce/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/albedo.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/510/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/zhongli.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/454/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/xinyan.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/72c/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/tartaglia.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/e84/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/diona.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/5bb/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/klee.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/f77/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/venti.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/edb/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/qiqi.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/049/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/mona.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/083/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/keqing.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/f4f/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/diluc.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/dcc/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/jean.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/1b4/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/amber.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/4c0/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/chongyun.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/0e0/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/fischl.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/535/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/xiangling.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/f7f/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/xingqiu.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/655/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/sucrose.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/a50/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/razor.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/56f/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/noelle.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/f2f/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/ningguang.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/5ea/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/lisa.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/c27/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/kaeya.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/582/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/baidou.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/018/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/bennett.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/954/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/barbara.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/1b2/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Traveler.webp'
-    ],
-    [
-        '/upload/resize_cache/iblock/1b2/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Traveler.webp'
-    ]
-]
+let character = {
+  "members": [
+    {
+      "name": "Юнь Дзинь",
+      "file": "character_1.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Паймон",
+      "file": "character_2.jpg",
+      "element": "paimon"
+    },
+    {
+      "name": "Кокоми",
+      "file": "character_3.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Итэр",
+      "file": "character_4.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Эмбер",
+      "file": "character_5.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Барбара",
+      "file": "character_6.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Бэй Доу",
+      "file": "character_7.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Бэннет",
+      "file": "character_8.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Чун Юнь",
+      "file": "character_9.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Дилюк",
+      "file": "character_10.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Фишль",
+      "file": "character_11.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Джинн",
+      "file": "character_12.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Кэйа",
+      "file": "character_13.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Кэ Цин",
+      "file": "character_14.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Кли",
+      "file": "character_15.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Лиза",
+      "file": "character_16.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Люмин",
+      "file": "character_17.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Мона",
+      "file": "character_18.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Нин Гуан",
+      "file": "character_19.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Ноэлль",
+      "file": "character_20.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Ци Ци",
+      "file": "character_21.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Рэйзор",
+      "file": "character_22.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Сахароза",
+      "file": "character_23.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Венти",
+      "file": "character_24.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Сян Лин",
+      "file": "character_25.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Сяо",
+      "file": "character_26.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Син Цю",
+      "file": "character_27.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Альбедо",
+      "file": "character_28.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Аяка",
+      "file": "character_29.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Аято",
+      "file": "character_30.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Гань Юй",
+      "file": "character_31.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Горо",
+      "file": "character_32.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Диона",
+      "file": "character_33.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Е Лань",
+      "file": "character_34.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Ёимия",
+      "file": "character_35.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Итто",
+      "file": "character_36.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Кадзуха",
+      "file": "character_37.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Куки",
+      "file": "character_38.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Райдэн",
+      "file": "character_39.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Розария",
+      "file": "character_40.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Сара",
+      "file": "character_41.jpg",
+      "element": "electro"
+    },
+    {
+      "name": "Саю",
+      "file": "character_42.jpg",
+      "element": "anemo"
+    },
+    {
+      "name": "Синь Янь",
+      "file": "character_43.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Тарталья",
+      "file": "character_44.jpg",
+      "element": "hydro"
+    },
+    {
+      "name": "Тома",
+      "file": "character_45.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Ху Тао",
+      "file": "character_46.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Джун Ли",
+      "file": "character_47.jpg",
+      "element": "geo"
+    },
+    {
+      "name": "Шэнь Хэ",
+      "file": "character_48.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Элой",
+      "file": "character_49.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Эола",
+      "file": "character_50.jpg",
+      "element": "cryo"
+    },
+    {
+      "name": "Янь Фей",
+      "file": "character_51.jpg",
+      "element": "piro"
+    },
+    {
+      "name": "Яэ Мико",
+      "file": "character_52.jpg",
+      "element": "electro"
+    },
+  ]
+}
 
 class Character {
-  constructor(charName, imgUrl) {
-    this.name = charName;
-    this.url = imgUrl;
+  constructor(name, file, element) {
+    this.name = name;
+    this.file = file;
+    this.element = element;
+    this.url = 'assets/images/characters/'
   }
 }
 
 let memberArray = [];
 
-// for (let i = 0; i < character['members'].length; i++) {
-//   let member = new Character(
-//     // character['members'][i]['name'],
-//     // character['members'][i]['url'],
-//     characterName[i],
-//     characterUrl[i]
-//   )
-  
-//   memberArray.push(member);
-// }
-
-for (let i = 0; i < characterName.length; i++) {
+for (let i = 0; i < character['members'].length; i++) {
   let member = new Character(
-    characterName[i],
-    'https://genshin-info.ru/' + characterUrl[i]
+    character['members'][i]['name'],
+    character['members'][i]['file'],
+    character['members'][i]['element']
   )
   
   memberArray.push(member);
 }
 
 Character.prototype.generation = function() {
+  imgHead.style.display = 'none';
+
   wrapper.innerHTML =
-    '<button class="btn" id="show">Показать персонажа</button>'+
-    '<img src="'+ this.url +'" class="character__img" alt="' + this.name + '" style="display: none;">'+
-    '<p class="character__text">'+ this.name +'</p>';
+    `<p class="character__text --color-${this.element}">${this.name}</p>
+    <img src="${this.url}${this.file}" class="character__img" alt="${this.name}" style="display: none;">
+    <div class="checkbox-wrapper">
+      <label for="show">
+        Показать фотографию
+      </label>
+      <div class="checkbox">
+        <input type="checkbox" id="show"><div class="checkmark"></div>
+      </div>
+    </div>
+    <div class="view">
+      <img src="${this.url}${this.file}" alt="" class="view__img">
+    </div>`;
   
-  let btnShow = document.querySelector('#show');
   
-  btnShow.addEventListener('click', () => {
-    let img = document.querySelector('.character__img');
+  let show = document.querySelector('#show'),
+      img = document.querySelector('.character__img'),
+      view  = document.querySelector('.view');
+
+  if (prevShow.checked) {
     img.style.display = 'block';
-    btnShow.remove();
+    img.onload = () => {
+      wrapper.scrollIntoView();
+    }
+    show.checked = true;
+  } else {
+    img.style.display = 'none';
+    show.checked = false;
+  }
+
+  let viewShow = () => {
+    view.style.display = 'block';
+  }
+
+  img.addEventListener('click', viewShow);
+
+  let viewClose = () => {
+    view.style.display = 'none';
+  }
+
+  view.addEventListener('click', viewClose);
+
+  let checkShow = () => {
+    if (show.checked) {
+      img.style.display = 'block'
+      wrapper.scrollIntoView();
+      console.log('checked')
+      prevShow.checked = true;
+    } else {
+      img.style.display = 'none'
+      prevShow.checked = false;
+    }
+  }
+
+  show.addEventListener('click', checkShow);
+
+  prevWrapper.style.display = 'none';
+
+  btn.addEventListener('click', () => {
+    show.removeEventListener('click', checkShow);
+    img.removeEventListener('click', viewShow);
+    view.removeEventListener('click', viewClose);
   }, { once: true })
 }
 
